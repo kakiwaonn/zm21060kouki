284bf7d72536a30bfed2c27efee907de6c13beda
kakiwaonn <131092193+kakiwaonn@users.noreply.github.com>
2023-10-18 11:59:24 +0900

    Update ex6-6.js

--- ex6-6.js
+++ ex6-6.js
@@ -1,6 +1,8 @@
 var who65='world';
 
 function sayhello66(){alert('Hello,'+who66+'!');}
+function saycome66(){alert(who66+'が来た');}
+
 function taro(){who66('太郎');}
 function hanako(){who66('花子');}
-function saycome66(){who66('が来た');}
+
