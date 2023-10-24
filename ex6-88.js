52ffe15d0e593805687333478dcb517ea18f3699
kakiwaonn <131092193+kakiwaonn@users.noreply.github.com>
2023-10-24 23:07:01 +0900

    Update ex6-88.js

--- ex6-88.js
+++ ex6-88.js
@@ -1,1 +1,1 @@
-function gazo688(){document.getElementById('who688').innerHTML=<img src"" alt="オン">}
+function gazo688(){document.getElementById('who688').innerHTML=<img src"cat_koubakozuwari_brown.png" alt="オン">}
