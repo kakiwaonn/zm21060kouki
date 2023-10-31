25ec0b97e910f11a7d11b0748c27a542a55e094e
kakiwaonn <131092193+kakiwaonn@users.noreply.github.com>
2023-10-31 10:35:21 +0900

    Update ex06-10.js

--- ex06-10.js
+++ ex06-10.js
@@ -1,1 +1,2 @@
 function imgchange1(){document.getElementById('logo').src='image/animal_cat_kowai.png';}
+function imgchange2(){document.getElementById('logo').src='image/cat_koubakozuwari_brown.png';}
