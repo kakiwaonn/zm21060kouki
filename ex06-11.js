6daed715adde2ec8fbf1b34fef76efa25a7deb24
kakiwaonn <131092193+kakiwaonn@users.noreply.github.com>
2023-11-07 10:25:37 +0900

    Update ex06-11.js

--- ex06-11.js
+++ ex06-11.js
@@ -1,2 +1,2 @@
-function imgchange3(){document.getElementById('logo2').src='';}
+function imgchange3(){document.getElementById('logo2').src='image/josai_logo.jpg';}
 function imgchange4(){document.getElementById('logo2').src='image/png-transparent-josai-university-hd-logo.png';}
