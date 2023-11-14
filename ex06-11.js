669e4c8709c0bd85db947d41321c203a01d61b36
kakiwaonn <131092193+kakiwaonn@users.noreply.github.com>
2023-11-14 09:58:12 +0900

    Update ex06-11.js

--- ex06-11.js
+++ ex06-11.js
@@ -1,1 +1,1 @@
-function hyoji(){//表示する文字 var str="一文字ずつ表示します。"; //テキストボックスの文字数 var cnt=document.timer.moji.value.length; //文字が全部表示されているか確認　if(cnt<11)}
+function hyoji(){//表示する文字 var str="一文字ずつ表示します。"; //テキストボックスの文字数 var cnt=document.timer.moji.value.length; //文字が全部表示されているか確認 if(cnt<11)}
