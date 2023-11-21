f0fa4cf04ec4184f7719371fb37df955b251af64
kakiwaonn <131092193+kakiwaonn@users.noreply.github.com>
2023-11-21 10:38:01 +0900

    Create ex06-14.js

---
+++ ex06-14.js
@@ -0,0 +1,2 @@
+function imgchange3(){document.getElementById('logo').src='image/animal_cat_kowai.png';}
+function imgchange4(){document.getElementById('logo').src='image/cat_koubakozuwari_brown.png';}
