379f68916f832ca3d61afd82cd3d372759e443e9
kakiwaonn <131092193+kakiwaonn@users.noreply.github.com>
2023-11-28 22:49:54 +0900

    Update ex07-3.js

--- ex07-3.js
+++ ex07-3.js
@@ -1,4 +1,5 @@
 function BMI(){
-var x=Number(document.getElementById('input3').value);
-var y=Number(document.getElementById('input4').value);
-alert(x/(y^2));}
+var input3(document.getElementById('input3').value);
+var input4(document.getElementById('input4').value);
+var BMI1=input3/(input4*input4);
+
