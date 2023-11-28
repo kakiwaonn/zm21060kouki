893b4d9ef49eaeced339b2830984d705e57b9cc2
kakiwaonn <131092193+kakiwaonn@users.noreply.github.com>
2023-11-28 14:31:16 +0900

    Update ex06-14.js

--- ex06-14.js
+++ ex06-14.js
@@ -1,3 +1,3 @@
-function imgchange5(){document.getElementById('logo3').src='image/animal_cat_kowai.png';}
-function imgchange6(){document.getElementById('logo3').src='image/cat_koubakozuwari_brown.png';}
+function gazou1(){document.getElementById('logo3').src='image/animal_cat_kowai.png';}
+function gazou2(){document.getElementById('logo3').src='image/cat_koubakozuwari_brown.png';}
 
