8fdbd2b956bd8c9d6879fb8ae1dd16d16d8a7662
kakiwaonn <131092193+kakiwaonn@users.noreply.github.com>
2023-11-28 10:42:39 +0900

    Update ex06-14.js

--- ex06-14.js
+++ ex06-14.js
@@ -1,21 +1,3 @@
 function imgchange5(){document.getElementById('logo3').src='image/animal_cat_kowai.png';}
 function imgchange6(){document.getElementById('logo3').src='image/cat_koubakozuwari_brown.png';}
 
-function hyoji(){//表示する元画像
-var gazou="image/cat_koubakozuwari_brown.png";
-
-function ()
-
-
-  
-  //テキストボックスの文字数
-var cnt=document.timer.moji.value.length;
-  //画像が全部表示されているか確認
-if(cnt<1){
-  //現在より1文字多く切り出して表示
-  document.timer.moji.value=str.substr(0,cnt+1);}
-else{
-  //全て表示されたら、空文字に戻す
-  document.timer.moji.value="";}}
-function startfnc(){//関数hyoji()を3000ミリ秒間隔で呼び出す
-  setInterval("hyoji()",3000);}
