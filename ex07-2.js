6c9f4084bbff5e89fcc80a57ea8b712200394688
kakiwaonn <131092193+kakiwaonn@users.noreply.github.com>
2023-11-28 20:34:07 +0900

    Update ex07-2.js

--- ex07-2.js
+++ ex07-2.js
@@ -1,4 +1,4 @@
 function add(){
-var x=document.getElemrntById('input1').value;
-var y=document.getEkementById('input2').value;
+var x=document.getElementById('input1').value;
+var y=document.getElementById('input2').value;
 alert(x+y);}
