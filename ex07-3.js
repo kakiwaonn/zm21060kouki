d2faad613472503f353eadf4b868bec2dc46c96f
kakiwaonn <131092193+kakiwaonn@users.noreply.github.com>
2023-11-29 00:03:26 +0900

    Update ex07-3.js

--- ex07-3.js
+++ ex07-3.js
@@ -2,4 +2,4 @@ function BMI(){
 var input3=Number(document.getElementById('input3').value);
 var input4=Number(document.getElementById('input4').value);
 var BMI1=input3/(input4*input4);
-alert('あなたのBMIは'+'BMI1'+'です。');
+alert('BMI1');
