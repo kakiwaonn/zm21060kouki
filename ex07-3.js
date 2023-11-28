6423d4ee6b96d17842c85c68c662872230de4fd0
kakiwaonn <131092193+kakiwaonn@users.noreply.github.com>
2023-11-29 00:59:25 +0900

    Update ex07-3.js

--- ex07-3.js
+++ ex07-3.js
@@ -2,4 +2,4 @@ function BMI(){
 var sinntyou=Number(document.getElementById('sinntyou').value);
 var taijyuu=Number(document.getElementById('taijyuu').value);
 var BMI1=taijyuu/sinntyou^2;
-alert('BMI1');
+alert('BMI1');}
