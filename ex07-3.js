91d4c9153a817b56afa4e4ab38c6f65935d322d6
kakiwaonn <131092193+kakiwaonn@users.noreply.github.com>
2023-11-29 19:58:59 +0900

    Update ex07-3.js

--- ex07-3.js
+++ ex07-3.js
@@ -1,6 +1,7 @@
 function BMI() {
- var x = Number(document.getElementById('sinntyou').value) / 100;
- var y = Number(document.getElementById('taijyuu').value);
- var bmi = y / (x * x);
- alert('bmi');}
+  var x = Number(document.getElementById('sinntyou').value) / 100; // 身長をメートルに変換
+  var y = Number(document.getElementById('taijyuu').value);
+  var bmi = y / (x * x); // BMIの計算
 
+    alert('bmi');
+}
