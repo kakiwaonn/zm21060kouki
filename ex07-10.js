255ea6b5ca6c2013e290c017d76d9b782c80ff33
kakiwaonn <131092193+kakiwaonn@users.noreply.github.com>
2023-11-30 10:57:23 +0900

    Update ex07-10.js

--- ex07-10.js
+++ ex07-10.js
@@ -1,4 +1,11 @@
+function BMI() {
+    var x = Number(document.getElementById('height').value) / 100; // メートルに
+    var y = Number(document.getElementById('weight').value);
+    var bmi = y / (x * x); // BMIの計算
 
+  
+    alert(bmi);
+}
 
 
 
