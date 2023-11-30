319a773daeee2438ba15879c77f44cd9962dd344
kakiwaonn <131092193+kakiwaonn@users.noreply.github.com>
2023-11-30 11:18:17 +0900

    Update ex07-10.js

--- ex07-10.js
+++ ex07-10.js
@@ -12,10 +12,3 @@ function BMI2() {
     
     alert(bmi2);
 }
-
-
-//身長：<input type="text" id="height2">
-<br>
-体重：<input type="text" id="weight2">
-<br>
-<input type="button" value="BMI計算" onclick="BMI2();"> 
