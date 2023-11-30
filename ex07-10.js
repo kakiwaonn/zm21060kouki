64283009cddd987bf7a31e2a87da6e362651124f
kakiwaonn <131092193+kakiwaonn@users.noreply.github.com>
2023-11-30 11:16:18 +0900

    Update ex07-10.js

--- ex07-10.js
+++ ex07-10.js
@@ -1,17 +1,20 @@
-function BMI() {
+function BMI2() {
     var x = Number(document.getElementById('height').value) / 100; // メートルに
     var y = Number(document.getElementById('weight').value);
-    var bmi = y / (x * x); // BMIの計算
+    var bmi2 = y / (x * x); // BMIの計算
 
     var siwake;
+  if(bmi2<18.5){
+       siwake='低体重';}
+  else if(bmi2<25){
+       siwake='普通';}
+  else {siwake='肥満';}  
     
-    
-    alert(bmi);
+    alert(bmi2);
 }
 
 
-
-身長：<input type="text" id="height2">
+//身長：<input type="text" id="height2">
 <br>
 体重：<input type="text" id="weight2">
 <br>
