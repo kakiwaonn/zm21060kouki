8a09628bfeea2f1c20133ec136b461bb07c820ee
kakiwaonn <131092193+kakiwaonn@users.noreply.github.com>
2023-12-01 02:45:36 +0900

    Update ex07-11.js

--- ex07-11.js
+++ ex07-11.js
@@ -6,7 +6,7 @@ if(nenn % 4==0 && nenn % 100 !==0 || nenn % 400==0 );}
 { tosi2='閏年';}
 else{tosi2='平年';}
 
-alert('西暦は'+nenn+'年は'+nenn+);}
+alert('西暦は'+nenn+'年は'+tosi2+);}
 
 
 //<p>西暦年<input type="text" id="seirekinenn"></p>
