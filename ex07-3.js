cefcb8844372c88913e533d2de2650ae7d75ce46
kakiwaonn <131092193+kakiwaonn@users.noreply.github.com>
2023-11-30 11:28:46 +0900

    Update ex07-3.js

--- ex07-3.js
+++ ex07-3.js
@@ -1,7 +1,8 @@
 function BMI() {
-    var x = Number(document.getElementById('height').value) / 100; // 身長をメートルに変換
+    var x = Number(document.getElementById('height').value) / 100; //メートル変換
     var y = Number(document.getElementById('weight').value);
-    var bmi = y / (x * x); // BMIの計算
+   
+    var bmi = y / (x * x); // BMI計算
 
     alert(bmi);
 }
