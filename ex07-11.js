d1399c98917a56fb0bab3e6b8c94c008778057c7
kakiwaonn <131092193+kakiwaonn@users.noreply.github.com>
2023-12-01 21:33:54 +0900

    Update ex07-11.js

--- ex07-11.js
+++ ex07-11.js
@@ -8,12 +8,6 @@ else {(tosi2='平年');}
   
 alert('西暦は'+nenn+'年は'+'tosi2');}
 
-  var siwake;
-  if(bmi2<18.5){
-       siwake='低体重';}
-  else if(bmi2<25){
-       siwake='普通';}
-  else {siwake='肥満';}
 
 //<p>西暦年<input type="text" id="seirekinenn"></p>
 //<input type="button" value="西暦年解答" onclick="tosi();">  
