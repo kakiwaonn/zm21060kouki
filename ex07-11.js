0da38b745e1b0108f1f037e26f7fa66099fb9e20
kakiwaonn <131092193+kakiwaonn@users.noreply.github.com>
2023-12-02 20:27:45 +0900

    Update ex07-11.js

--- ex07-11.js
+++ ex07-11.js
@@ -1,14 +1,14 @@
 function tosi(){
-var nenn=Number(document.getElementById('seirekinenn').value);
+var nenn = Number(document.getElementById('seirekinenn').value);
 var tosi2;
   
-if((nenn % 4 == 0 && nenn % 100 != 0 )|| nenn % 400 == 0)
+if ((nenn % 4 == 0 && nenn % 100 != 0 )|| nenn % 400 == 0)
 { tosi2='閏年';
 }else{
- tosi2='平年';
+ tosi2 = '平年';
 }
   
-alert('西暦は'+nenn+'年は'+'tosi2');}
+alert('西暦は' +nenn+ '年は' + 'tosi2');}
 }
 
 
