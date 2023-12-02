a00ef70be16db2fa1923c7ca706885ffce73fd8b
kakiwaonn <131092193+kakiwaonn@users.noreply.github.com>
2023-12-02 14:05:53 +0900

    Update ex07-11.js

--- ex07-11.js
+++ ex07-11.js
@@ -2,9 +2,9 @@ function tosi(){
 var nenn=document.getElementById('seirekinenn').value;
 var tosi2;
   
-if(nenn % 4==0 && nenn % 100 !==0 || nenn % 400==0);
+if(nenn % 4 == 0 && nenn % 100 != 0 || nenn % 400 == 0);
 { tosi2='閏年';}
-if else { tosi2='平年';}
+else { tosi2='平年';}
   
 alert('西暦は'+nenn+'年は'+'tosi2');}
 
@@ -12,6 +12,7 @@ alert('西暦は'+nenn+'年は'+'tosi2');}
 //<p>西暦年<input type="text" id="seirekinenn"></p>
 //<input type="button" value="西暦年解答" onclick="tosi();">  
 
-//倫理演算子
+//倫理演算子 
+//余剰 document.write(15 % 4);
 //4で割り切れる　400でも割り切れる　閏年
 // 100で割り切れ400で割り切れない　平年 
